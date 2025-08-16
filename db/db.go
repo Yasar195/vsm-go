138ac5b884e09c607f6a34556c8939927a933802
Yasar195 <imyasar07@gmail.com>
2025-08-16 10:13:03 +0530

    added connection optimization for lanbda envitonment

--- db/db.go
+++ db/db.go
@@ -21,68 +21,70 @@ var DB *gorm.DB
 func ConnectDatabase() {
 	dsn := os.Getenv("DATABASE_URL")
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// err = db.Migrator().DropTable(&schema.Users{})
-	// if err != nil {
-	// 	println("Warning: Could not drop Users table:", err.Error())
-	// }
-
-	err = db.AutoMigrate(&schema.Users{}, &schema.Visitor{})
-
-	if err != nil {
-		panic("failed to migrate database schema")
-	}
-
-	var count int64
-	result := db.Model(&schema.Users{}).Count(&count)
-
-	if result.Error != nil {
-		panic("failed to count Users table rows")
-	}
-
-	if count == 0 {
-		println("No users found, inserting default user...")
-
-		emailConfig := utility.EmailConfig{
-			SMTPHost:     os.Getenv("SMTP_HOST"),
-			SMTPPort:     587,
-			SMTPUsername: os.Getenv("ADMIN_EMAIL"),
-			SMTPPassword: os.Getenv("ADMIN_PASSWORD"),
-			FromEmail:    os.Getenv("ADMIN_EMAIL"),
+	if DB == nil {
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
 		}
 
-		emailService := utility.NewEmailService(emailConfig)
+		// err = db.Migrator().DropTable(&schema.Users{})
+		// if err != nil {
+		// 	println("Warning: Could not drop Users table:", err.Error())
+		// }
 
-		err := emailService.SendEmail(os.Getenv("ADMIN_EMAIL"), "admin created", "Hi\nNew admin create\n\nemail: imyasar07@gmail.com\npassword: admin@123")
+		err = db.AutoMigrate(&schema.Users{}, &schema.Visitor{})
 
 		if err != nil {
-			panic("error sending email")
+			panic("failed to migrate database schema")
 		}
 
-		hash, err := utility.HashPassword("admin@123")
+		var count int64
+		result := db.Model(&schema.Users{}).Count(&count)
 
-		if err != nil {
-			panic("hashing password failed")
+		if result.Error != nil {
+			panic("failed to count Users table rows")
 		}
 
-		defaultUser := schema.Users{
-			Username:  "admin",
-			UserEmail: "imyasar07@gmail.com",
-			Password:  hash,
-			UserType:  "staff",
+		if count == 0 {
+			println("No users found, inserting default user...")
+
+			emailConfig := utility.EmailConfig{
+				SMTPHost:     os.Getenv("SMTP_HOST"),
+				SMTPPort:     587,
+				SMTPUsername: os.Getenv("ADMIN_EMAIL"),
+				SMTPPassword: os.Getenv("ADMIN_PASSWORD"),
+				FromEmail:    os.Getenv("ADMIN_EMAIL"),
+			}
+
+			emailService := utility.NewEmailService(emailConfig)
+
+			err := emailService.SendEmail(os.Getenv("ADMIN_EMAIL"), "admin created", "Hi\nNew admin create\n\nemail: imyasar07@gmail.com\npassword: admin@123")
+
+			if err != nil {
+				panic("error sending email")
+			}
+
+			hash, err := utility.HashPassword("admin@123")
+
+			if err != nil {
+				panic("hashing password failed")
+			}
+
+			defaultUser := schema.Users{
+				Username:  "admin",
+				UserEmail: "imyasar07@gmail.com",
+				Password:  hash,
+				UserType:  "staff",
+			}
+
+			err = db.Create(&defaultUser).Error
+
+			if err != nil {
+				panic("error creating default user")
+			}
 		}
 
-		err = db.Create(&defaultUser).Error
-
-		if err != nil {
-			panic("error creating default user")
-		}
+		DB = db
+		fmt.Println("✅ Database connected")
 	}
-
-	DB = db
-	fmt.Println("✅ Database connected")
 }
